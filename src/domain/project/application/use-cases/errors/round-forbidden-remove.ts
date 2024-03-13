941e1ebbd2d97b948a219bca315ed36f42abcf72
Yuri Muniz <y.munizgonzalez@gmail.com>
2024-03-12 23:01:48 -0300

    remove-round

---
+++ src/domain/project/application/use-cases/errors/round-forbidden-remove.ts
@@ -0,0 +1,7 @@
+import { UseCaseError } from '@/core/errors/use-case-error'
+
+export class RoundForbiddenRemoveError extends Error implements UseCaseError {
+  constructor() {
+    super(`Não é possível excluir o round.`)
+  }
+}
