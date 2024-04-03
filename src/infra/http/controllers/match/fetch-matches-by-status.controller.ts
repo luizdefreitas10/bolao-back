e411b2aa383ed2fe0c8462a5fbc32077e1d3fefb
Luiz de Freitas <luizdefreitas@MacBook-Pro-de-Luiz.local>
2024-04-03 14:29:14 -0300

    feat: initial code for fetch matches by status

---
+++ src/infra/http/controllers/match/fetch-matches-by-status.controller.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { ZodValidationPipe } from "../../pipes/zod-validation-pipe";
+import { ApiTags } from "@nestjs/swagger";
+import { BadRequestException, Controller, Get, HttpCode, Param, Query } from "@nestjs/common";
+import { Roles } from "@/infra/auth/roles.decorator";
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1));
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
+
+@ApiTags("match")
+@Controller("/match/:roundId/status")
+export class FetchMatchByStatusController {
+  constructor(private fetchMatchesByStatus: FetchMatchesByStatus) {}
+
+  @Get()
+  @HttpCode(200)
+  @Roles(["ADMIN"])
+  async handle(
+    @Query("page", queryValidationPipe) page: PageQueryParamSchema,
+    @Param("roundId") roundId: number
+  ) {
+    const result = await this.fetchMatchesByStatus.execute({
+      page,
+      roundId,
+    });
+
+    if (result.isLeft()) {
+          throw new BadRequestException();
+      }
+    }
+
+    const { match } = result.value;
+
+    return { matchId: match.id.toString() };
+  }
+}
